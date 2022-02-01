43728dd0d5fd8978c9778b43da0d58131f10ac8d
zatr <zatr@inftecpro.com>
2022-01-31 22:14:36 -0600

    Correct references in  pipeline for aws_tools

--- artifacts.tf
+++ artifacts.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "aws_tester_build_artifacts" {
     Application = "aws_tester"
   }
 }
+
+resource "aws_s3_bucket" "aws_tools_build_artifacts" {
+  bucket = "itp-cicd-build-artifacts"
+  acl = "private"
+
+  tags = {
+    Application = "aws_tools"
+  }
+}
--- pipeline.tf
+++ pipeline.tf
@@ -12,8 +12,8 @@ module "cicd_aws_tools_main" {
   source = "./pipeline"
   repository = "aws_tools"
   branch = "main"
-  artifact_bucket = aws_s3_bucket.aws_tester_build_artifacts.bucket
-  application_tag = "aws_tester"
+  artifact_bucket = aws_s3_bucket.aws_tools_build_artifacts.bucket
+  application_tag = "aws_tools"
   aws_region = var.aws_region
   aws_account_id = var.aws_account_id
 }
