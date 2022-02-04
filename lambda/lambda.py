51edb8231e2272dc7adf151bd1718185b36c0626
zatr <zatr@inftecpro.com>
2022-02-03 20:20:03 -0600

    Make Lambda function handle event, add logging

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,5 +1,15 @@
 import json
+import logging
+import boto3
 
 
-def start_test_runner(software_version):
-    print("Received event: " + software_version)
+def start_test_runner(event, context):
+    job_id = event['CodePipeline.job']['id']
+    print(f"Received event: CodePipeline.job: {job_id}")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.debug(json.dumps(event))
+
+    codepipeline = boto3.client('codepipeline')
+    response = codepipeline.put_job_success_result(jobId=job_id)
+    logger.debug(response)
